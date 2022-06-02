12dd38856e6098b8e567bff6fae25c0359334d69
Lubov <lubaparfyonova@gmail.com>
2022-06-02 15:34:03 +0300

    init reposite

---
+++ sequelize/routes/groupRouter.js
@@ -0,0 +1,44 @@
+const { Router } = require('express');
+const GroupController = require('../controllers/group.controller');
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.resolve(__dirname, '../public/images'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}.${file.originalname}`);
+  }
+});
+
+const upload = multer({ storage });
+
+const groupRouter = Router();
+
+groupRouter.post('/', GroupController.createUserGroup);
+groupRouter.post(
+  '/:groupId/image',
+  upload.single('image'),
+  GroupController.createImage
+);
+groupRouter.get('/:userId', GroupController.getUserGroups);
+groupRouter.put('/:groupId', GroupController.addUserGroup);
+
+module.exports = groupRouter;
+
+/*
+Написать маршрут и контроллер для добавления 
+юзера в группу
+
+1) Принимаем id юзера и группы 
+(юзера в req.body, groupId - в req.params)
+2) Найти юзера
+3) Найти группу
+4) *****Make magic********
+5) Добавляем юзера в группу
+6) Возвращаем группу со всеми юзерами в ответ
+
+
+
+*/
