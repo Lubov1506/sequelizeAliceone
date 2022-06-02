be33e82b3a23aa84984a6d8105868b55631ecb78
Lubov <lubaparfyonova@gmail.com>
2022-06-02 17:30:36 +0300

    Patg refact

--- sequelize/models/index.js
+++ sequelize/models/index.js
@@ -5,7 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const {DB_CONFIG} = require('../config');
+const config = require(DB_CONFIG)[env];
 const db = {};
 
 let sequelize;
